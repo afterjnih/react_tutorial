c2edf0302ebdfdfafc4084804fce86fb02090edc
Koji Ota <afterjnih@gmail.com>
2015-02-07 16:12:40 +0900

    tutorial13

--- server.rb
+++ server.rb
@@ -7,29 +7,21 @@
 # FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 require 'webrick'
 require 'json'
-
-comments = react_version = JSON.parse(File.read('./_comments.json'))
-
 puts 'Server started: http://localhost:3000/'
-
 root = File.expand_path './public'
 server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
-
 server.mount_proc '/comments.json' do |req, res|
-  if req.request_method == 'POST'
-    # Assume it's well formed
-    comments << req.query
-    File.write('./_comments.json', comments.to_json)
-  end
-
-  # always return json
-  res['Content-Type'] = 'application/json'
-  res.body = comments.to_json
+comments = JSON.parse(File.read('./_comments.json'))
+if req.request_method == 'POST'
+# Assume it's well formed
+comments << req.query
+File.write('./_comments.json', JSON.pretty_generate(comments, :indent => ' '))
 end
-
+# always return json
+res['Content-Type'] = 'application/json'
+res.body = JSON.generate(comments)
+end
 trap 'INT' do server.shutdown end
-
 server.start
